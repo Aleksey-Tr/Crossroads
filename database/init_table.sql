60fbcf3596535819758d00883422e2fb00c34dc3
Aleksey-Tr <tretyakofaleksey@gmail.com>
2026-08-08 21:15:21 +1000

    fix syntax

--- database/init_table.sql
+++ database/init_table.sql
@@ -1,3 +1,4 @@
+-- Active: 1785762682836@@127.0.0.1@5432@novels_test_db
 DROP DATABASE IF EXISTS novels_test_db;
 
 CREATE DATABASE novels_test_db;
@@ -10,19 +11,19 @@ CREATE TABLE users (
     login VARCHAR(32) UNIQUE NOT NULL,
     nickname VARCHAR(32) UNIQUE NOT NULL,
     password VARCHAR(16) NOT NULL, --только для тестов
-    password_hash VARCHAR() NOT NULL
+    password_hash VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
-    author_id INTEGER FOREIGN KEY REFERENCES users (id) NOT NULL,
+    author_id INTEGER REFERENCES users (id) NOT NULL,
     title VARCHAR(64) NOT NULL,
     description TEXT NULL,
     published_at DATE
 );
 
 CREATE TABLE characters (
-    book_id INTEGER NOT NULL FOREIGN KEY REFERENCES books (id),
+    book_id INTEGER NOT NULL REFERENCES books (id),
     character_id INTEGER NOT NULL,
     title VARCHAR(16) NULL, --если есть нумерация глава 1, глава 2, то оставить NULL иначе NOT NULL
     content TEXT NOT NULL,
